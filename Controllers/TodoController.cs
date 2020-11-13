776ca0fc7021b5d3f457788fe9bb3cb42eabd119
jordan.woodruff <jordan.woodruff@tylertech.com>
2020-11-13 17:01:05 -0600

    a complete MVC example

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -2,12 +2,26 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AspNetCoreTodo.Models;
+using AspNetCoreTodo.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AspNetCoreTodo.Controllers
 {
     public class TodoController : Controller
     {
+        private readonly ITodoItemService _todoItemService;
 
+        public TodoController(ITodoItemService todoItemService)
+        {
+            _todoItemService = todoItemService;
+        }
+        
+        public async Task<IActionResult> Index()
+        {
+            var items = await _todoItemService.GetIncompleteItemsAsync();
+            
+            return View(new TodoViewModel{Items = items});
+        }
     }
 }
---
+++ Services/FakeTodoItemService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AspNetCoreTodo.Models;
+
+namespace AspNetCoreTodo.Services
+{
+    public class FakeTodoItemService : ITodoItemService
+    {
+        public Task<List<TodoItem>> GetIncompleteItemsAsync()
+        {
+            var list = new List<TodoItem>
+            {
+                new TodoItem
+                {
+                    Title = "Learn ASP.NET Core",
+                    DueAt = DateTimeOffset.Now.AddDays(1)
+                },
+                new TodoItem
+                {
+                    Title = "Build awesome apps",
+                    DueAt = DateTimeOffset.Now.AddDays(2)
+                }
+
+            };
+
+            return Task.FromResult(list);
+        }
+    }
+}
